57361f043c7eb0b4494178711b9ba58105d7fd49
rimbabazudewa <bazudewa34@gmail.com>
2023-03-30 19:08:51 +0800

    add new fe-runner

---
+++ fe-runner/_next/static/cZV2k23VRiVA-IC-9y5oI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/868-5de02d8562a8ddc5.js","static/css/5075c9957baacc04.css","static/chunks/pages/index-12db3d87faacbf49.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
